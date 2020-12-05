9b281a704f2c17d7a8e9982d7917e3e303671629
Guilherme Goncalves <guilhermegufes@gmail.com>
2020-12-04 22:07:14 -0300

    partial build of analyzer

---
+++ lexical_analyzer.py
@@ -0,0 +1,377 @@
+TOKENS = {
+    'HST': '#',
+    'MENOR': '<',
+    'MAIOR': '>',
+    'ATRIB': "=",
+
+    'AP': '(',
+    'FP': '(',
+    'ACH': '{',
+    'FCH': '}',
+    'ASP': '"',
+    'PNTVIRG': ';',
+    'DPT': ':',
+    'ESPACO': ' ',
+    'SEPARADOR': "\n\r\t",
+
+    'VARIAVEL': "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdeghijklnopqrstuvwxyz_",
+    'DIGITO': "0123456789",
+
+    'INCLUDE': "include",
+    'MAIN': "main",
+
+    'INT': "int",
+    'FLOAT': "float",
+    'CHAR': "char",
+
+    'SWITCH': "switch",
+    'CASE': "case",
+    'BREAK': "break",
+    'RETURN': "return",
+    'PRINTF': "printf"
+}
+
+recognizedTokens = []
+
+
+def nextCharacterIs( token, nextCharacter  ):
+    return TOKENS[token].find(nextCharacter)
+
+def state_q0( nextCharacter, file, recognizedTokens ):
+    if( nextCharacter == '#'):
+        recognizedTokens.append('HST')
+
+    elif( nextCharacter == '<'):
+        recognizedTokens.append('MENOR')
+
+    elif( nextCharacter == '>'):
+        recognizedTokens.append('MAIOR')
+
+    elif( nextCharacter == '('):
+        nextCharacter = file.read(1)
+        recognizedTokens.append('AP')
+        state_q0(nextCharacter, file, recognizedTokens)
+
+    elif( nextCharacter == ')'):
+        recognizedTokens.append('FP')
+
+    elif( nextCharacter == '{'):
+        recognizedTokens.append('ACH')
+    
+    elif( nextCharacter == '}'):
+        recognizedTokens.append('FCH')
+    
+    elif( nextCharacter == '"'):
+        recognizedTokens.append('ASP')
+
+    elif( nextCharacter == ';'):
+        recognizedTokens.append('PNTVIRG')
+
+    elif( nextCharacter == ':'):
+        recognizedTokens.append('DPT')
+
+    elif( nextCharacter == '='):
+        recognizedTokens.append('ATRIB')
+
+    elif( nextCharacter == 'c'):
+        nextCharacter = file.read(1)
+        state_c2(nextCharacter, file, recognizedTokens)
+    
+    elif( nextCharacter == 'i'):
+        nextCharacter = file.read(1)
+        state_q2(nextCharacter, file, recognizedTokens)
+        
+    elif( nextCharacter == 's'):
+        nextCharacter = file.read(1)
+        state_s2(nextCharacter, file, recognizedTokens)
+
+    elif( nextCharacter == 'm'):
+        nextCharacter = file.read(1)
+        state_m2(nextCharacter, file, recognizedTokens)
+
+    elif( nextCharacter == 'p'):
+        nextCharacter = file.read(1)
+        state_p2(nextCharacter, file, recognizedTokens)
+    
+    elif( nextCharacter == 'r'):
+        nextCharacter = file.read(1)
+        state_r2(nextCharacter, file, recognizedTokens)
+
+    elif( nextCharacter == 'b'):
+        nextCharacter = file.read(1)
+        state_b2(nextCharacter, file, recognizedTokens)
+
+    elif( (nextCharacterIs( 'VARIAVEL', nextCharacter) != -1) and nextCharacter != 'b' and nextCharacter != 'c' and nextCharacter != 'i' and nextCharacter != 'm' and nextCharacter != 's' and nextCharacter != 'p' and nextCharacter != 'r' ):
+        nextCharacter = file.read(1)
+        state_v1(nextCharacter, file, recognizedTokens)
+        recognizedTokens.append("VARIAVEL")
+        # nextCharacter = file.read(1)    
+        # state_q0(nextCharacter, file, recognizedTokens)
+    
+
+def state_v0(nextCharacter, file, recognizedTokens):
+    nextCharacter = file.read(1)
+    if( nextCharacterIs( 'VARIAVEL', nextCharacter) != -1 ):
+        recognizedTokens.pop()
+        recognizedTokens.append("VARIAVEL")
+        # state_v0(nextCharacter, file, recognizedTokens)
+    
+    state_q0(nextCharacter, file, recognizedTokens)
+    
+def state_v1(nextCharacter, file, recognizedTokens):
+    if( nextCharacterIs( 'VARIAVEL', nextCharacter) != -1):
+        nextCharacter = file.read(1)
+        state_v1(nextCharacter, file, recognizedTokens)
+    
+
+def state_q2(nextCharacter, file, recognizedTokens ):
+    if( nextCharacter == 'n'):
+        nextCharacter = file.read(1)
+        state_q3(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'n' ):
+        state_v0( nextCharacter, file, recognizedTokens )  
+
+def state_q3(nextCharacter, file, recognizedTokens ):
+    if( nextCharacter == 'c'):
+        nextCharacter = file.read(1)
+        state_q4(nextCharacter, file, recognizedTokens)
+    elif( nextCharacter == 't'):
+        recognizedTokens.append('INT')
+        state_v0(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'w' ):
+        state_v0( nextCharacter, file, recognizedTokens )          
+
+def state_q4(nextCharacter, file, recognizedTokens ):
+    if( nextCharacter == 'l'):
+        nextCharacter = file.read(1)
+        state_q5(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'l' ):
+        state_v0( nextCharacter, file, recognizedTokens )  
+
+def state_q5(nextCharacter, file, recognizedTokens ):
+    if( nextCharacter == 'u'):
+        nextCharacter = file.read(1)
+        state_q6(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'u' ):
+        state_v0( nextCharacter, file, recognizedTokens )  
+
+def state_q6(nextCharacter, file, recognizedTokens ):
+    if( nextCharacter == 'd'):
+        nextCharacter = file.read(1)
+        state_q7(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'd' ):
+        state_v0( nextCharacter, file, recognizedTokens )  
+
+def state_q7(nextCharacter, file, recognizedTokens ):
+    if( nextCharacter == 'e'):
+        # nextCharacter = file.read(1)
+        recognizedTokens.append('INCLUDE')        
+        state_v0(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'e' ):
+        state_v0( nextCharacter, file, recognizedTokens )  
+
+
+def state_s2(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'w'):
+        nextCharacter = file.read(1)
+        state_s3(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'w' ):
+        state_v0( nextCharacter, file, recognizedTokens )    
+
+def state_s3(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'i'):
+        nextCharacter = file.read(1)
+        state_s4(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'i' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_s4(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 't'):
+        nextCharacter = file.read(1)
+        state_s5(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 't' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_s5(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'c'):
+        nextCharacter = file.read(1)
+        state_s6(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'c' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_s6(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'h'):
+        recognizedTokens.append('SWITCH')  
+        state_v0(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'h' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+
+def state_m2(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'a'):
+        nextCharacter = file.read(1)
+        state_m3(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'a' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_m3(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'i'):
+        nextCharacter = file.read(1)
+        state_m4(nextCharacter, file, recognizedTokens)
+    elif( nextCharacterIs(('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'i' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_m4(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'n'):
+        recognizedTokens.append('MAIN')  
+        state_v0(nextCharacter, file, recognizedTokens)     
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'n' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+
+def state_p2(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'r'):
+        nextCharacter = file.read(1)
+        state_p3(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'r' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_p3(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'i'):
+        nextCharacter = file.read(1)
+        state_p4(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'i' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_p4(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'n'):
+        nextCharacter = file.read(1)
+        state_p5(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'n' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_p5(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 't'):
+        nextCharacter = file.read(1)
+        state_p6(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 't' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_p6(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'f'):
+        recognizedTokens.append('PRINTF')
+        state_v0(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'f' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+
+def state_r2(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'e'):
+        nextCharacter = file.read(1)
+        state_r3(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'e' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_r3(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 't'):
+        nextCharacter = file.read(1)
+        state_r4(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 't' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_r4(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'u'):
+        nextCharacter = file.read(1)
+        state_r5(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'u' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_r5(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'r'):
+        nextCharacter = file.read(1)
+        state_r6(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'r' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_r6(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'n'):
+        recognizedTokens.append('RETURN')
+        state_v0(nextCharacter, file, recognizedTokens)
+    
+
+def state_b2(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'r'):
+        nextCharacter = file.read(1)
+        state_b3(nextCharacter, file, recognizedTokens)
+
+def state_b3(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'e'):
+        nextCharacter = file.read(1)
+        state_b4(nextCharacter, file, recognizedTokens)
+
+def state_b4(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'a'):
+        nextCharacter = file.read(1)
+        state_b5(nextCharacter, file, recognizedTokens)
+
+def state_b5(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'k'):
+        recognizedTokens.append('BREAK')
+        state_v0(nextCharacter, file, recognizedTokens)
+
+
+
+def state_c2(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'h'):
+        nextCharacter = file.read(1)
+        state_c3(nextCharacter, file, recognizedTokens)
+    elif( nextCharacter == 'a'):
+        nextCharacter = file.read(1)
+        state_c6(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'h' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_c3(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'a'):
+        nextCharacter = file.read(1)
+        state_c4(nextCharacter, file, recognizedTokens)
+
+def state_c4(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'r'):
+        recognizedTokens.append('CHAR')
+        state_v0(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 'r' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+
+def state_c6(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 's'):
+        nextCharacter = file.read(1)
+        state_c7(nextCharacter, file, recognizedTokens)
+    elif( (nextCharacterIs('VARIAVEL', nextCharacter) != -1) and nextCharacter != 's' ):
+        state_v0( nextCharacter, file, recognizedTokens )
+
+def state_c7(nextCharacter, file, recognizedTokens):
+    if( nextCharacter == 'e'):
+        recognizedTokens.append('CASE')
+        state_v0( nextCharacter, file, recognizedTokens )
+
+
+
+try:
+    file = open('inputfile.txt', 'r')
+
+    while True:
+        nextCharacter = file.read(1)
+
+        # print(nextCharacter,'.')
+        state_q0( nextCharacter, file, recognizedTokens)
+        if(nextCharacter == ""):
+            recognizedTokens.append('EOF')
+            break
+        
+    file.close()
+except:
+    print("Error opening file")
+
+print(recognizedTokens)
