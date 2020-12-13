e9d181316cd2dad70873e3ef4922275cb6871462
Guilherme Goncalves <guilhermegufes@gmail.com>
2020-12-13 14:52:00 -0300

    Final code

--- grammar.py
+++ grammar.py
@@ -1,149 +1,171 @@
 import scanner
 import states
 import TOKENS
-
-
-
+import sys
 
 file = scanner.scanner()
 positionFile = 0
 recognizedToken = ''
 
-
-# def nextTokenIs(recognizedToken):
-#     recognizedToken
 def ignoreSpace(positionFile):
-    while file[positionFile] ==  "\n" or file[positionFile] ==  " " or file[positionFile] ==  "\t" or file[positionFile] ==  "" :
-        positionFile += 1
-    return positionFile
-
+        while positionFile < len(file) and (file[positionFile] in ["\n", " ", "\t"]):
+            positionFile += 1
+        return positionFile
+    
 def nextToken(positionFile, file, recognizedToken):
-        print(positionFile,"RR")
-        print(positionFile)
         positionFile = ignoreSpace(positionFile)
-        print(positionFile)
+        if positionFile == len(file):
+            exit()
         positionFile, recognizedToken = states.state_q0(positionFile, file, recognizedToken)
         return  positionFile, recognizedToken
 
 def recognize( token, positionFile,  recognizedToken ):
+    actualToken = token
     if token == recognizedToken:
         print("TokenReconhecido", token)
         positionFile, recognizedToken = nextToken( positionFile, file, recognizedToken)
-        print( positionFile,"-->", recognizedToken)
+        print( "nextToken","-->", recognizedToken)
         return positionFile, recognizedToken
-       
+    else:
+        print("TokenEsperado:", actualToken)
+        # return positionFile, recognizedToken
 
 def init(positionFile, recognizedToken):
-    positionFile, recognizedToken = nextToken(positionFile, file, recognizedToken)
-    # print(positionFile, recognizedToken)
-    positionFile, recognizedToken = includeLib(positionFile, recognizedToken)
-    positionFile, recognizedToken = mainFunction(positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.EOF, positionFile, recognizedToken)
-
-
-def includeLib(positionFile, recognizedToken):
-    positionFile, recognizedToken = includeCommand(positionFile, recognizedToken)
-    positionFile, recognizedToken = includeCommand(positionFile, recognizedToken)
-    # print(positionFile, recognizedToken)
-    return positionFile, recognizedToken 
+    try:    
+        positionFile, recognizedToken = includeLibrary(positionFile, recognizedToken)
+        positionFile, recognizedToken = mainFunction(positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
+def includeLibrary(positionFile, recognizedToken):
+    try:
+        positionFile, recognizedToken = includeCommand(positionFile, recognizedToken)
+        positionFile, recognizedToken = includeCommand(positionFile, recognizedToken)
+        return positionFile, recognizedToken 
+    except:
+        exit()
 
 def includeCommand(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.HST, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.INCLUDE, positionFile, recognizedToken)
-    
-    positionFile, recognizedToken = recognize(TOKENS.MENOR, positionFile, recognizedToken)
-    # positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.MAIOR, positionFile, recognizedToken)
-    return positionFile, recognizedToken
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.HST, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.INCLUDE, positionFile, recognizedToken)
+        
+        positionFile, recognizedToken = recognize(TOKENS.MENOR, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.MAIOR, positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 def mainFunction(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.INT, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.MAIN, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.AP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.FP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ACH, positionFile, recognizedToken)
-    positionFile, recognizedToken = mainBody(positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.FCH, positionFile, recognizedToken)
-    return positionFile, recognizedToken
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.INT, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.MAIN, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.AP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.FP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ACH, positionFile, recognizedToken)
+        positionFile, recognizedToken = mainBody(positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.FCH, positionFile, recognizedToken)
+
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 
 def mainBody(positionFile, recognizedToken):
-    positionFile, recognizedToken = commandBlock(positionFile, recognizedToken)
-    positionFile, recognizedToken = returnCommand(positionFile, recognizedToken)
-    return positionFile, recognizedToken
-    # positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
+    try:
+        positionFile, recognizedToken = commandBlock(positionFile, recognizedToken)
+        positionFile, recognizedToken = returnCommand(positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 def commandBlock(positionFile, recognizedToken):
-    # declaration()
-    positionFile, recognizedToken = declaration(positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
-    positionFile, recognizedToken = attribution(positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
-    positionFile, recognizedToken = switchCommand(positionFile, recognizedToken)
-    # positionFile, recognizedToken =printfCommand(positionFile, recognizedToken)
-
-# def returnCommand():
-#     recognize(TOKENS.NUMERO)
-    return positionFile, recognizedToken
+    try:
+        positionFile, recognizedToken = declaration(positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
+        positionFile, recognizedToken = attribution(positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
+        positionFile, recognizedToken = switchCommand(positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 def switchCommand(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.SWITCH, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.AP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken )
-    positionFile, recognizedToken = recognize(TOKENS.FP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ACH, positionFile, recognizedToken)
-    positionFile, recognizedToken = caseBlock(positionFile, recognizedToken)
-    positionFile, recognizedToken = caseBlock(positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.FCH, positionFile, recognizedToken)
-    return positionFile, recognizedToken
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.SWITCH, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.AP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken )
+        positionFile, recognizedToken = recognize(TOKENS.FP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ACH, positionFile, recognizedToken)
+        positionFile, recognizedToken = caseBlock(positionFile, recognizedToken)
+        positionFile, recognizedToken = caseBlock(positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.FCH, positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 def declaration(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.CHAR, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
-    return positionFile, recognizedToken
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.CHAR, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 def attribution(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ATRIB, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ATRIB, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
 
-    return positionFile, recognizedToken
 
 def caseBlock(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.CASE, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.DPT, positionFile, recognizedToken)
-    positionFile, recognizedToken = printfCommand(positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.BREAK, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.CASE, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ASPS, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.DPT, positionFile, recognizedToken)
+        positionFile, recognizedToken = printfCommand(positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.BREAK, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
 
-    return positionFile, recognizedToken
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 def printfCommand(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.PRINTF, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.AP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ASP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.ASP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.FP, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.PRINTF, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.AP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ASP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.VARIAVEL, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.ASP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.FP, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
 
-    return positionFile, recognizedToken
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 def returnCommand(positionFile, recognizedToken):
-    positionFile, recognizedToken = recognize(TOKENS.RETURN, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.NUMERO, positionFile, recognizedToken)
-    positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
-    return positionFile, recognizedToken
+    try:
+        positionFile, recognizedToken = recognize(TOKENS.RETURN, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.NUMERO, positionFile, recognizedToken)
+        positionFile, recognizedToken = recognize(TOKENS.PNTVIRG, positionFile, recognizedToken)
+        return positionFile, recognizedToken
+    except:
+        exit()
 
 
-print(file[260] == "\n")
+# print(file)
+positionFile, recognizedToken = nextToken(positionFile, file, recognizedToken)
 init(positionFile, recognizedToken)
-# positionFile, recognizedToken = nextToken(positionFile, file, recognizedToken)
-# if file[26] == " ":
+
 
